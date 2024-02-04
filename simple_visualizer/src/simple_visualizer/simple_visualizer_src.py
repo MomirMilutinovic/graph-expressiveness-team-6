4668b8adadb48bf80a5641d35474f1e8cfd2ea3c
Momir Milutinović <milutinm462@gmail.com>
2024-02-04 19:23:22 +0100

    #20 fix: Fix issues with entry points and template loading in simple visualizer

--- simple_visualizer/src/simple_visualizer/simple_visualizer_src.py
+++ simple_visualizer/src/simple_visualizer/simple_visualizer_src.py
@@ -1,4 +1,5 @@
-from jinja2 import Environment, PackageLoader
+from jinja2 import Environment, FileSystemLoader
+import os
 
 
 # TODO: Remove when real implementation of graph is available
@@ -12,12 +13,14 @@ class Graph:
 
 class MockGraph(Graph):
     def get_nodes(self):
-        return [{"id": str(i)} for i in range(300)]
+        return [{"id": str(i)} for i in range(20)]
 
     def get_edges(self):
         edges = []
-        for i in range(200):
-            for j in range(200):
+        for i in range(10):
+            for j in range(10):
+                if i == j:
+                    continue
                 edges.append({
                     "src": {"id": str(i)},
                     "dest": {"id": str(j)}
@@ -27,14 +30,15 @@ class MockGraph(Graph):
 
 class SimpleVisualizer():
     def __init__(self):
-        self.environment = Environment(loader=PackageLoader("simple_visualizer", package_path="templates/"))
+        path=os.path.join(os.path.dirname(__file__))
+        self.environment = Environment(loader=FileSystemLoader(path))
         self.template = self.environment.get_template("simple_visualizer_template.html")
 
     def get_name(self):
         return "simple_visualizer"
 
     def display(self, graph: Graph) -> str:
-        return self.template.render(nodes=graph.get_nodes(), edges=graph.get_edges())
+        return self.template.render(nodes=graph.get_nodes(), edges=graph.get_edges(), name=self.get_name())
 
 
 if __name__ == "__main__":
