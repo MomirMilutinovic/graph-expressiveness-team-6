17fd82cb923c838f9dd45f7af64fe5d586d87669
Vladimir Popov <vladapopov02@gmail.com>
2024-02-04 02:12:50 +0100

    #14 feat: implement model api domain methods and fix imports

--- api/models/graph.py
+++ api/models/graph.py
@@ -1,21 +1,23 @@
-from edge import Edge
-from node import Node
+from .edge import Edge
+from .node import Node
 
 
 class Graph:
 
-    def __init__(self, edges: list[Edge], nodes: list[Node]):
+    def __init__(self, edges: list[Edge], nodes: set[Node] = {}):
         self.edges = edges
         self.nodes = nodes
 
-    def remove_node(self):
-        pass
+    def remove_node(self, node: Node):
+        self.nodes.remove(node)
 
     def add_node(self, node: Node):
-        pass
+        self.nodes.add(node)
 
     def add_edge(self, edge: Edge):
-        pass
+        self.add_node(edge.src)
+        self.add_node(edge.dest)
+        self.edges.append(edge)
 
     def get_nodes(self) -> list[Node]:
-        pass
+        return self.nodes
--- api/models/node.py
+++ api/models/node.py
@@ -1,12 +1,20 @@
-from edge import Edge
+from .edge import Edge
 
 
 class Node:
 
-    def __init__(self, id_: str, data: dict, edges: list[Edge]):
-        self.id = id_
+    def __init__(self, id: str, data: dict, edges: list[Edge] = []):
+        self.id = id
         self.data = data
         self.edges = edges
 
     def get_neighbours(self):
-        pass
+        neighbours: list[Node] = []
+
+        for edge in self.edges:
+            if edge.dest == self:
+                neighbours.append(edge.src)
+            else:
+                neighbours.append(edge.dest)
+
+        return neighbours
