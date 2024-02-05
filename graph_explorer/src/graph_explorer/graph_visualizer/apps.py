7333149a03fb28809ac1a26d229e9837dceae59d
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-02-05 03:33:23 +0100

    #25 feat: Load class from entry points

--- graph_explorer/src/graph_explorer/graph_visualizer/apps.py
+++ graph_explorer/src/graph_explorer/graph_visualizer/apps.py
@@ -3,7 +3,7 @@ from pkg_resources import iter_entry_points
 
 
 def load_plugins(entry_point_name):
-    return [x.load() for x in iter_entry_points(entry_point_name)]
+    return [x.load()() for x in iter_entry_points(entry_point_name)]
 
 
 class GraphVisualizerConfig(AppConfig):
