c39996267a75f082e286d6db36b39f2894730a10
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-02-05 03:45:31 +0100

    #25 feat: Add typings and revert toml

--- graph_explorer/src/graph_explorer/graph_visualizer/apps.py
+++ graph_explorer/src/graph_explorer/graph_visualizer/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 from pkg_resources import iter_entry_points
 
+from api.components.data_source import DataSource
+from api.components.visualizer import Visualizer
+
 
 def load_plugins(entry_point_name):
     return [x.load()() for x in iter_entry_points(entry_point_name)]
@@ -10,8 +13,8 @@ class GraphVisualizerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'graph_visualizer'
 
-    data_source_plugins = []
-    visualizer_plugins = []
+    data_source_plugins: list[DataSource] = []
+    visualizer_plugins: list[Visualizer] = []
 
     def ready(self):
         self.data_source_plugins = load_plugins("datasources")
