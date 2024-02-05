a963689894f9a193558de540790afd20bf220fd4
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-02-05 09:51:06 +0100

    #27 feat: Reimplement index method to fetch data from content module

--- graph_explorer/src/graph_explorer/graph_visualizer/views.py
+++ graph_explorer/src/graph_explorer/graph_visualizer/views.py
@@ -1,29 +1,27 @@
 from django.shortcuts import render
+from django.apps.registry import apps
+
+from .module.content import ContentModule
+
+content_module = ContentModule(apps.get_app_config('graph_visualizer').data_source_plugins,
+                        apps.get_app_config('graph_visualizer').visualizer_plugins)
 
 
 def index(request):
-    return render(request, 'index.html', {
-        "workspaces": [
-            {
-                "name": "Workspace 1",
-                "id": 1
-            },
-            {
-                "name": "Workspace 2",
-                "id": 2
-            }
-        ],
-        "visualizers": [
-            {
-                "name": "Simple visualizer",
-                "id": 1
-            },
-            {
-                "name": "Block visualizer",
-                "id": 2
-            }
-        ],
-    })
+    context = content_module.get_context()
+    context["workspaces"] = [
+
+        {
+            "name": "Workspace 1",
+            "id": 1
+        },
+        {
+            "name": "Workspace 2",
+            "id": 2
+        }
+    ]
+
+    return render(request, 'index.html', context)
 
 
 def workspace(request, workspace_id):
