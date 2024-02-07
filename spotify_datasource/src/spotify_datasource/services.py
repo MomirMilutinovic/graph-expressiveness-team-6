1260a9ef3481cb4e5a81bf6225455c31704e8734
Vladimir Popov <vladapopov02@gmail.com>
2024-02-07 22:32:12 +0100

    #6 fix: datasource processed nodes and breaking when max_neighbours is reached

--- spotify_datasource/src/spotify_datasource/services.py
+++ spotify_datasource/src/spotify_datasource/services.py
@@ -11,16 +11,11 @@ def get_graph(
     recursion_depth: int = 2,
 ) -> Graph:
     artist = get_artist_by_name(auth_token, artist_name)
-<<<<<<< HEAD
-    graph = Graph()
-    procesed_artist_ids = set()
-=======
     graph = Graph(directed=False)
     processed_artist_ids = set()
->>>>>>> 36d2793 (#22 feat: add support for newly created graph api)
 
     process_artists_recursively(
-        auth_token, artist, max_neighbours, recursion_depth, graph, procesed_artist_ids
+        auth_token, artist, max_neighbours, recursion_depth, graph, processed_artist_ids
     )
 
     return graph
@@ -32,24 +27,28 @@ def process_artists_recursively(
     max_neighbours: int,
     recursion_depth: int,
     graph: Graph,
-    procesed_artist_ids: set[str],
+    processed_artist_ids: set[str],
 ):
     if recursion_depth == 0:
         return
 
-    if artist.id in procesed_artist_ids:
+    if artist.id in processed_artist_ids:
         return
 
     rel_artists = get_related_artists(auth_token, artist.id, max_neighbours)
+    processed_artist_ids.add(artist.id)
 
-    for rel_artist in rel_artists:
+    for i, rel_artist in enumerate(rel_artists):
+        if i > max_neighbours - 1:
+            break
+
         process_artists_recursively(
             auth_token,
             rel_artist,
             max_neighbours,
             recursion_depth - 1,
             graph,
-            procesed_artist_ids,
+            processed_artist_ids,
         )
 
         src = Node(artist.id, vars(artist))
@@ -57,4 +56,3 @@ def process_artists_recursively(
         edge = Edge({}, src, dest)
 
         graph.add_edge(edge)
-        procesed_artist_ids.add(artist.id)
