7fc48737d3faead27a2bfaab86ef5304968fa95f
Vladimir Popov <vladapopov02@gmail.com>
2024-02-10 00:27:13 +0100

    #14 feat: add python docstrings to api component classes

--- api-sok/src/api/components/visualizer.py
+++ api-sok/src/api/components/visualizer.py
@@ -3,10 +3,27 @@ from ..models.graph import Graph
 
 
 class Visualizer(ABC):
+    """Abstract class representing the API of the Visualizer plugins.
+    In order to create a Visualizer plugin you have to implement this class with all its methods and follow the instructions on how to create a plugin toml file in the readme of the repository: https://github.com/MomirMilutinovic/graph-expressiveness-team-6
+    """
+
     @abstractmethod
     def get_name(self) -> str:
+        """Fetches the display name of the plugin.
+
+        Returns:
+            str: Plugin display name
+        """
         pass
 
     @abstractmethod
-    def display(self, graph: Graph):
+    def display(self, graph: Graph) -> str:
+        """Provides an HTML representation of the Graph that is ready to be presented to the end user.
+
+        Args:
+            Graph: Graph that is to be displayed
+
+        Returns:
+            str: HTML representation of the graph
+        """
         pass
