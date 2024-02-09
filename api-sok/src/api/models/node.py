051114fcd40cefc90accc84cc61745c72e2742a5
Vladimir Popov <vladapopov02@gmail.com>
2024-02-10 00:48:06 +0100

    #14 feat: add docstrings to node model in api

--- api-sok/src/api/models/node.py
+++ api-sok/src/api/models/node.py
@@ -1,13 +1,35 @@
 class Node:
+    """
+    A class representing a node in a graph.
+
+    Attributes:
+        id (str): The identifier of the node.
+        data (dict): A dictionary containing additional data associated with the node.
+        edges (list): A list of edges incident to the node.
+    """
 
     def __init__(self, id: str, data: dict, edges=None):
+        """
+        Initializes a Node object.
+
+        Args:
+            id (str): The identifier of the node.
+            data (dict): A dictionary containing additional data associated with the node.
+            edges (list, optional): A list of edges incident to the node. Defaults to None.
+        """
         if edges is None:
             edges = []
         self.id = id
         self.data = data
         self.edges = edges
 
-    def get_neighbours(self) -> list['Node']:
+    def get_neighbours(self) -> list["Node"]:
+        """
+        Retrieves the neighboring nodes of the current node.
+
+        Returns:
+            list[Node]: A list of neighboring nodes.
+        """
         neighbours: list[Node] = []
 
         for edge in self.edges:
@@ -17,14 +39,38 @@ class Node:
         return neighbours
 
     def __eq__(self, __value: object) -> bool:
+        """
+        Checks if the current node is equal to another node.
+
+        Args:
+            __value (object): The object to compare with.
+
+        Returns:
+            bool: True if the nodes are equal, False otherwise.
+        """
         if isinstance(__value, Node):
             return self.id == __value.id
         return False
 
     def __hash__(self) -> int:
+        """
+        Computes the hash value of the node.
+
+        Returns:
+            int: The hash value of the node.
+        """
         return hash(self.id)
 
     def __contains__(self, item):
+        """
+        Checks if the node contains the specified item.
+
+        Args:
+            item: The item to check for containment.
+
+        Returns:
+            bool: True if the item is contained in the node's id or data, False otherwise.
+        """
         return (
             item in self.id
             or any(item in str(value) for value in self.data.values())
